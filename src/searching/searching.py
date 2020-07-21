57b228fb90997a1006c60429f0e918b36d851937
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-21 16:15:00 -0700

    finished both, made both O(1) memory

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,17 +1,15 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, *args):
+def binary_search(arr, target, start, end):
     # Your code here
-    l = len(arr)
-    m = l//2
-    if l == 0:
+    l = end - start
+    m = start + l//2
+    if end < start:
         return -1
-    elif target == arr[m]:
-        return m
-    elif target < arr[m]:
-        return binary_search(arr[:m], target)
-    elif target > arr[m]:
-        r = binary_search(arr[m:], target)
-        return r + m * (r >= 0)
+    return m if target == arr[m] else (
+    binary_search(arr,
+                  target,
+                  start * (target < arr[m]) + m * (target > arr[m]),
+                  end * (target > arr[m]) + m * (target < arr[m])))
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -20,6 +18,19 @@ def binary_search(arr, target, *args):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-    pass
+def agnostic_binary_search(arr, target, start=0, end=None, ascending=None):
+# Your code here
+    if end is None:
+        end = len(arr) - 1
+        ascending = arr[start] < arr[end]
+    l = end - start
+    m = start + l//2
+    if end < start:
+        return -1
+    l = ascending ^ (target < arr[m])
+    n = not l
+    return m if target == arr[m] else (
+    agnostic_binary_search(arr,
+                  target,
+                  start * n + (m + 1) * l,
+                  (m - 1) * n + end * l, ascending))
